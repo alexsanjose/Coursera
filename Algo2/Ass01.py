1f6a4deec2a9714bb99350ad85102a5f6b0d2a29
Alex Joseph <alexsanjoseph@gmail.com>
2014-07-09 18:43:18 +0530

    First commit of assignments

--- Algo2/Ass01.py
+++ Algo2/Ass01.py
@@ -1,6 +1,14 @@
 import sys
+import csv
 
-a = 1 +  (2**200)
 
-print a;
 
+with open('/home/alex2/Learning/Data/Algo2/jobs.txt', 'rb') as f:
+    reader = list(csv.reader(f))
+    row_count = sum(1 for row in reader)
+
+print len(reader)
+
+print range(0, len(reader))
+print reader[2]
+print sum(reader[i] for i in range(0, len(reader)))
