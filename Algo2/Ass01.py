ff663e20d320bf10ef1d799a6e6f164fa751b8a6
Alex Joseph <alexsanjoseph@gmail.com>
2014-07-18 16:07:46 +0530

    Assignment02 problem 1

--- Algo2/Ass01.py
+++ Algo2/Ass01.py
@@ -1,7 +1,11 @@
 import csv
 import numpy
+import urllib2
+
+f = urllib2.urlopen('http://spark-public.s3.amazonaws.com/algo2/datasets/jobs.txt') # read only 20 000 chars
+
 # f =  open('/home/alex2/Learning/Data/Algo2/jobs.txt', 'rb')
-f =  open('C:/Users/Alex/Learning/Data/Algo2/jobs.txt', 'rb')
+# f =  open('C:/Users/Alex/Learning/Data/Algo2/jobs.txt', 'rb')
 
 reader = list(csv.reader(f,  delimiter=' '))
 
@@ -10,6 +14,7 @@ jobs_length = numpy.array([int(reader[i][1]) for i in range(1, len(reader))])
 
 greed = jobs_weight - jobs_length
 
+print greed
 temp = greed.argsort()
 ranks = numpy.empty(len(array), int)
 ranks[temp] = numpy.arange(len(array))
